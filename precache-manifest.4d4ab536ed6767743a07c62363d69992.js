2982f01de1a50a2a9f33378b1e0be501335d919f
MaxGenash <max.genash@gmail.com>
2019-02-18 02:10:33 +0200

    Updates

---
+++ precache-manifest.4d4ab536ed6767743a07c62363d69992.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d06346ee4cee003638",
+    "url": "/MaxGenash/students-list-test-app/static/js/runtime~main.84d06346.js"
+  },
+  {
+    "revision": "a35d8d11676e07329a9e",
+    "url": "/MaxGenash/students-list-test-app/static/js/main.a35d8d11.chunk.js"
+  },
+  {
+    "revision": "544fab581b6f4a26f362",
+    "url": "/MaxGenash/students-list-test-app/static/js/2.544fab58.chunk.js"
+  },
+  {
+    "revision": "945411523db2f1e98b7eab238c92b92a",
+    "url": "/MaxGenash/students-list-test-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MaxGenash/students-list-test-app/precache-manifest.4d4ab536ed6767743a07c62363d69992.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MaxGenash/students-list-test-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
