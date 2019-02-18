8ac7656d3a735fd96b9ced4967fab698ca760453
MaxGenash <max.genash@gmail.com>
2019-02-18 02:34:38 +0200

    Updates

---
+++ precache-manifest.2a054bf6dd89fe23dadcf5be2b97bf29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "177b03f706eae085fe79",
+    "url": "/static/js/main.177b03f7.chunk.js"
+  },
+  {
+    "revision": "544fab581b6f4a26f362",
+    "url": "/static/js/2.544fab58.chunk.js"
+  },
+  {
+    "revision": "47a3fc59aabdd6e0f7b5eba4b411f7b2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MaxGenash/students-list-test-app/precache-manifest.bbeca89aab91858a105ae4df2d5e0f06.js"
+  "/precache-manifest.2a054bf6dd89fe23dadcf5be2b97bf29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MaxGenash/students-list-test-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
